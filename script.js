35c349986d1e440e8bdad73dbcf3eca3a3bcd928
iwatts3519 <iwatts@me.com>
2021-04-07 14:45:48 +0100

    Updated

---
+++ script.js
@@ -0,0 +1,26 @@
+let question = {
+    title: 'gato',
+    alternatives: ['dog', 'cat', 'bird', 'fish'],
+    correctAnswer: 1
+};
+
+function showQuestion(q) {
+    let titleDiv = document.getElementById('title');
+    titleDiv.textContent = q.title;
+
+    let alts = document.querySelectorAll('.alternative');
+    alts.forEach(function(element, index) {
+        element.textContent = q.alternatives[index];
+        element.addEventListener('click', function(){
+            if(q.correctAnswer == index) {
+                console.log("Correct Answer");
+            }
+            else {
+                console.log("Wrong Answer");
+            }
+        })
+    });
+}
+
+showQuestion(question)
+
